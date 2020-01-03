b501a9b33e943860e1c56e7811a337a590b1c39c
Dzhem <dzhem.aptula@gmail.com>
2020-01-03 12:14:15 +0100

    Rebuilding project

--- README.md
+++
@@ -1,4 +0,0 @@
-# Project COMMIT
-Welcome to project COMMIT in which Merijn, Bert, Dzhem and Nick Code Mastermind Together.
-In this GitHub repository you'll find all the files.
-We're group G19.
--- src/persistentie2/Connectie.java
+++
@@ -1,7 +0,0 @@
-package persistentie2;
-import java.sql.*; 
-// @author Nick De Loor (nickjedl)
-
-public class Connectie {
-    public static final String JDBC_URL = "jdbc:mysql://ID222177_g19.db.webhosting.be?user=ID222177_g19&password=H1pcidro";
-}
